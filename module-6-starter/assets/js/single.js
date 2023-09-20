f4740221cdd3f5f0f3418ad83b6188f042653286
abbyd <abbydiaz300@gmail.com>
2023-09-19 19:40:02 -0700

    added extra message if more than 30 issues in a repo

--- module-6-starter/assets/js/single.js
+++ module-6-starter/assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector('#issues-container');
+var limitWarningEl = document.querySelector('#limit-warning');
 
 var getRepoIssues = function(repo) {
 
@@ -9,6 +10,11 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 // pass response data to dom function
                 displayIssues(data);
+
+                // check is api has paginated issues
+                if(response.headers.get('Link')) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert('There was a problem with the request');
@@ -58,4 +64,19 @@ var displayIssues = function(issues) {
 
 };
 
-getRepoIssues("AbyDiaz/git-it-done");
+var displayWarning = function(repo) {
+
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues visit ";
+
+    var linkEl = document.createElement('a');
+    linkEl.textContent = ' GitHub.com';
+    linkEl.setAttribute('href', 'https://github.com/' + repo + '/issues');
+    linkEl.setAttribute('target', '_blank');
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+    
+};
+
+getRepoIssues("facebook/react");
